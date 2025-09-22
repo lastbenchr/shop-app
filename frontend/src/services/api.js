42d6bad0fe86696a866a72e53ae5ded99602afdb
Sooraj Yadav <sooraj0032@gmail.com>
2025-09-22 18:05:54 +0530

    cloud api url added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
-const base = import.meta.env.VITE_API_URL || "http://192.168.1.3:5000/api";
+// const base = import.meta.env.VITE_API_URL || "http://192.168.1.3:5000/api";
+const base =
+  import.meta.env.VITE_API_URL || "https://shop-app-lemon-tau.vercel.app/api";
 const api = axios.create({ baseURL: base });
 export default api;
